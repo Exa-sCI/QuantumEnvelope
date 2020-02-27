45fb07a75bc2853ab411870131e512c07cc25709
Swann Perarnau <swann@anl.gov>
2020-02-27 17:38:22 -0600

    Add notebook to visualize matrix patterns
    + nix shell environment

---
+++ shell.nix
@@ -0,0 +1,18 @@
+# development shell, includes all dependencies, debug, style tools
+{
+  pkgs ? import <nixpkgs> {}
+}:
+with pkgs;
+pkgs.mkShell {
+        name = "miniqp";
+        nativeBuildInputs = [];
+        buildInputs = [
+          # dependencies for the code
+          python3
+          python3Packages.black
+          python3Packages.notebook
+          python3Packages.matplotlib
+          python3Packages.ipywidgets
+          python3Packages.tqdm
+        ];
+}

2a1cc8aa95ec953edfbbe664339a1f4a23e09c01
tapplencourt <tapplencourt@anl.gov>
2022-05-31 14:36:13 -0500

    Clean Repo

--- nix/mnemo.nix
+++
@@ -1,8 +0,0 @@
-{ stdenv, pkgconfig, fetchgit, autoreconfHook, python3, src }:
-
-stdenv.mkDerivation {
-      name = "mnemo";
-      src = src;
-      nativeBuildInputs = [ autoreconfHook pkgconfig ];
-      buildInputs = [ python3 ];
-}
--- qpx/__init__.py
+++
@@ -1,2 +0,0 @@
-from .types import *
-from .input import *

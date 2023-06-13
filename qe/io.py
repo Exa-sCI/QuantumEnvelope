9810cd2c71dd599d4d89720b9271a8e6bc882c8b
Nathan Johnson <nathan.johnson@c357697l.cels.anl.gov>
2023-06-13 14:23:20 -0500

    Only master handels the files

--- qe/io.py
+++ qe/io.py
@@ -22,6 +22,7 @@ import math
 # ~
 def load_integrals(
     fcidump_path,
+    comm,
 ) -> Tuple[int, float, One_electron_integral, Two_electron_integral]:
     """Read all the Hamiltonian integrals from the data file.
     Returns: (E0, d_one_e_integral, d_two_e_integral).
@@ -31,117 +32,130 @@ def load_integrals(
     """
     import glob
 
-    if len(glob.glob(fcidump_path)) == 1:
-        fcidump_path = glob.glob(fcidump_path)[0]
-    elif len(glob.glob(fcidump_path)) == 0:
-        print("no matching fcidump file")
-    else:
-        print(f"multiple matches for {fcidump_path}")
-        for i in glob.glob(fcidump_path):
-            print(i)
-
-    # Use an iterator to avoid storing everything in memory twice.
-    if fcidump_path.split(".")[-1] == "gz":
-        import gzip
-
-        f = gzip.open(fcidump_path)
-    elif fcidump_path.split(".")[-1] == "bz2":
-        import bz2
-
-        f = bz2.open(fcidump_path)
-    else:
-        f = open(fcidump_path)
-
-    # Only non-zero integrals are stored in the fci_dump.
-    # Hence we use a defaultdict to handle the sparsity
-    n_orb = int(next(f).split()[2])
-
-    for _ in range(3):
-        next(f)
-
-    d_one_e_integral = defaultdict(int)
-    d_two_e_integral = defaultdict(int)
-
-    for line in f:
-        v, *l = line.split()
-        v = float(v)
-        # Transform from Mulliken (ik|jl) to Dirac's <ij|kl> notation
-        # (swap indices)
-        i, k, j, l = list(map(int, l))
-
-        if i == 0:
-            E0 = v
-        elif j == 0:
-            # One-electron integrals are symmetric (when real, not complex)
-            d_one_e_integral[
-                (i - 1, k - 1)
-            ] = v  # index minus one to be consistent with determinant orbital indexing starting at zero
-            d_one_e_integral[(k - 1, i - 1)] = v
+    rank = comm.Get_rank()
+    #only master handles file
+    if(rank == 0):
+        if len(glob.glob(fcidump_path)) == 1:
+            fcidump_path = glob.glob(fcidump_path)[0]
+        elif len(glob.glob(fcidump_path)) == 0:
+            print("no matching fcidump file")
         else:
-            # Two-electron integrals have many permutation symmetries:
-            # Exchange r1 and r2 (indices i,k and j,l)
-            # Exchange i,k
-            # Exchange j,l
-            key = compound_idx4(i - 1, j - 1, k - 1, l - 1)
-            d_two_e_integral[key] = v
+            print(f"multiple matches for {fcidump_path}")
+            for i in glob.glob(fcidump_path):
+                print(i)
 
-    f.close()
+        # Use an iterator to avoid storing everything in memory twice.
+        if fcidump_path.split(".")[-1] == "gz":
+            import gzip
 
-    return n_orb, E0, d_one_e_integral, d_two_e_integral
+            f = gzip.open(fcidump_path)
+        elif fcidump_path.split(".")[-1] == "bz2":
+            import bz2
+
+            f = bz2.open(fcidump_path)
+        else:
+            f = open(fcidump_path)
+
+        # Only non-zero integrals are stored in the fci_dump.
+        # Hence we use a defaultdict to handle the sparsity
+        n_orb = int(next(f).split()[2])
+
+        for _ in range(3):
+            next(f)
+
+        d_one_e_integral = defaultdict(int)
+        d_two_e_integral = defaultdict(int)
+
+        for line in f:
+            v, *l = line.split()
+            v = float(v)
+            # Transform from Mulliken (ik|jl) to Dirac's <ij|kl> notation
+            # (swap indices)
+            i, k, j, l = list(map(int, l))
+
+            if i == 0:
+                E0 = v
+            elif j == 0:
+                # One-electron integrals are symmetric (when real, not complex)
+                d_one_e_integral[
+                    (i - 1, k - 1)
+                ] = v  # index minus one to be consistent with determinant orbital indexing starting at zero
+                d_one_e_integral[(k - 1, i - 1)] = v
+            else:
+                # Two-electron integrals have many permutation symmetries:
+                # Exchange r1 and r2 (indices i,k and j,l)
+                # Exchange i,k
+                # Exchange j,l
+                key = compound_idx4(i - 1, j - 1, k - 1, l - 1)
+                d_two_e_integral[key] = v
+        #pack all the variables into tuple so it can be sent by master
+        tup = (n_orb, E0, d_one_e_integral, d_two_e_integral)
+        f.close()
+    else:
+        tup = ()
+        
+    return comm.bcast(tup, 0) 
 
 
-def load_wf(path_wf) -> Tuple[List[float], List[Determinant]]:
+def load_wf(path_wf, comm) -> Tuple[List[float], List[Determinant]]:
     """Read the input file :
     Representation of the Slater determinants (basis) and
     vector of coefficients in this basis (wave function)."""
 
     import glob
 
-    if len(glob.glob(path_wf)) == 1:
-        path_wf = glob.glob(path_wf)[0]
-    elif len(glob.glob(path_wf)) == 0:
-        print(f"no matching wf file: {path_wf}")
+    rank = comm.Get_rank()
+    #only master handles file
+    if(rank == 0): 
+        if len(glob.glob(path_wf)) == 1:
+            path_wf = glob.glob(path_wf)[0]
+        elif len(glob.glob(path_wf)) == 0:
+            print(f"no matching wf file: {path_wf}")
+        else:
+            print(f"multiple matches for {path_wf}")
+            for i in glob.glob(path_wf):
+                print(i)
+
+        if path_wf.split(".")[-1] == "gz":
+            import gzip
+
+            with gzip.open(path_wf) as f:
+                data = f.read().decode().split()
+        elif path_wf.split(".")[-1] == "bz2":
+            import bz2
+
+            with bz2.open(path_wf) as f:
+                data = f.read().decode().split()
+        else:
+            with open(path_wf) as f:
+                data = f.read().split()
+
+        def decode_det(str_):
+            for i, v in enumerate(str_):
+                if v == "+":
+                    yield i
+
+        def grouper(iterable, n):
+            "Collect data into fixed-length chunks or blocks"
+            args = [iter(iterable)] * n
+            return zip(*args)
+
+        det = []
+        psi_coef = []
+        for (coef, det_i, det_j) in grouper(data, 3):
+            psi_coef.append(float(coef))
+            det.append(Determinant(tuple(decode_det(det_i)), tuple(decode_det(det_j))))
+
+        # Normalize psi_coef
+
+        norm = math.sqrt(sum(c * c for c in psi_coef))
+        psi_coef = [c / norm for c in psi_coef]
+
+        tup = (psi_coef, det)
     else:
-        print(f"multiple matches for {path_wf}")
-        for i in glob.glob(path_wf):
-            print(i)
+        tup = None
 
-    if path_wf.split(".")[-1] == "gz":
-        import gzip
-
-        with gzip.open(path_wf) as f:
-            data = f.read().decode().split()
-    elif path_wf.split(".")[-1] == "bz2":
-        import bz2
-
-        with bz2.open(path_wf) as f:
-            data = f.read().decode().split()
-    else:
-        with open(path_wf) as f:
-            data = f.read().split()
-
-    def decode_det(str_):
-        for i, v in enumerate(str_):
-            if v == "+":
-                yield i
-
-    def grouper(iterable, n):
-        "Collect data into fixed-length chunks or blocks"
-        args = [iter(iterable)] * n
-        return zip(*args)
-
-    det = []
-    psi_coef = []
-    for (coef, det_i, det_j) in grouper(data, 3):
-        psi_coef.append(float(coef))
-        det.append(Determinant(tuple(decode_det(det_i)), tuple(decode_det(det_j))))
-
-    # Normalize psi_coef
-
-    norm = math.sqrt(sum(c * c for c in psi_coef))
-    psi_coef = [c / norm for c in psi_coef]
-
-    return psi_coef, det
+    return comm.bcast(tup, 0) 
 
 
 def load_eref(path_ref) -> Energy:
@@ -177,3 +191,4 @@ def load_eref(path_ref) -> Energy:
     import re
 
     return float(re.search(r"E +=.+", data).group(0).strip().split()[-1])
+
